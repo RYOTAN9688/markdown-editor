49deec1a4b7609b33fc18602f37d9d8e1e78adbd
RYOTAN9688 <ryota8813@gmail.com>
2021-07-22 18:47:00 +0900

    HTMLをサニタイズする

--- src/worker/convent_markdown_worker.ts
+++ src/worker/convent_markdown_worker.ts
@@ -1,4 +1,6 @@
 import * as marked from "marked";
+import * as sanitizeHtml from "sanitize-html";
+
 //Web Workerを変数にセット
 //typeScriptの型定義の兼ね合いで,self as anyと書くことで型チェックを回避する
 //workerはWorkerという型と定義することでWebWorkerの処理を正しく型チェックできるようになる
@@ -7,8 +9,10 @@ const worker: Worker = self as any;
 worker.addEventListener("message", (e) => {
     //メインスレッドからのテキストデータ（マークダウン）を
     const text = e.data;
-    //markedでhtmlに変換
-    const html = marked(text);
+    //sanitize（危険なコードやデータを変換または除去して無力化する処理）されたHTMLを出力
+    //オプションで渡しているallwedTagsは許可するHTMLのタグを列挙する
+    //sanitizeHtmlではh1,h2はデフォルトで除外されているので追加している。
+    const html = sanitizeHtml(marked(text), { allowedTags: [...sanitizeHtml.defaults.allowedTags, "h1", "h2"] })
     //メインスレッドに結果のhtmlを返却する
     worker.postMessage({ html })
 
