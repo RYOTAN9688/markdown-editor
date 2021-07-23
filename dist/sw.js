87f95342c128e676c2c53c0c908f9b8ff8ebcebe
RYOTAN9688 <ryota8813@gmail.com>
2021-07-23 13:35:42 +0900

    サービスワーカー　ネットワークリクエストに介入する

---
+++ dist/sw.js
@@ -0,0 +1,48 @@
+//キャッシュの名前を定義。キャッシュAPIは、このキャッシュに応じて別のキャッシュを
+//提供してくれる。例えば一度キャッシュの内容をリセットしたい場合は、キャッシュ名を変更することで新しい状態にできる。
+//キャッシュ名は任意の文字列でよいが、バージョンがわかるように定義したほうが良い
+const CacheName = "Cache:v1"
+
+self.addEventListener("install", (event) => {
+    console.log("ServiceWorker install:", event);
+})
+
+self.addEventListener("activate", (event) => {
+    console.log("ServiceWorker activate:", event);
+})
+
+
+const networkFallingBackToCache = async (request) => {
+    //定義した名前でキャッシュを開く
+    //cacheインターフェイスは、Request / Responseオブジェクトのペアのためのストレージの仕組みを提供する
+    //リクエストに対してレスポンスを保持してくれるシンプルな仕組みでキャッシュを提供してくれる
+    const cache = await caches.open(CacheName)
+    try {
+        //通常のfetchリクエストを実行してレスポンスを取得する
+        const response = await fetch(request)
+        //レスポンス内容をキャッシュに保存している。なおresponse.clone()でレスポンスの内容をこぴーしてから保存しなければならない。
+        //これはレスポンスの内部で一度しか読み取りできない処理があるため。
+        await cache.put(request, response.clone())
+        //レスポンスを呼び出しもとに返却
+        return response;
+        //リクエストにエラーが発生した場合、
+    } catch (err) {
+        //コンソールにエラーを出す
+        console.error(err);
+        //キャッシュの内容を返却する
+        return caches.match(request)
+    }
+}
+
+//Fetchイベント時に実行する処理を登録。fetchとはネットワーク
+//を経由してリソースを取得するために使用するapi
+self.addEventListener("fetch", (event) => {
+    //リクエストの内容を表示。
+    console.log("Fetch to:", event.request.url);
+    //ネットワークリクエストを行って結果をメインスレッドに戻す処理
+    //event.respondWithは簡単に言うと、非同期処理(Promise)の
+    //実行終了まで待機してくれる
+    event.respondWith(fetch(event.request))
+})
+
+
